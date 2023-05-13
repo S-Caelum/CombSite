5ae1d9d5c3f65fc7a812e24b0d2070f80f5a4c16
S-Caelum <deus.caelum@outlook.com>
2023-05-13 11:03:56 +0500

    Optimized 83.1% of project

--- src/pages/api/user/employeeTime.ts
+++ src/pages/api/user/employeeTime.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../utils/prisma';
+import { prisma } from '../../../utils/prisma';
 import moment from 'moment';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const date = req.body;
 }
